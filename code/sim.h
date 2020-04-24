7e035e4f6ad0f1a38d7b670d3c54a9412202d04f
Yiyuan Chen <yiyuanc@unix7.andrew.cmu.edu>
2020-04-24 16:25:10 -0400

    added .h file

---
+++ code/sim.h
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <math.h>
+
+
+typedef struct {
+
+    int rc; // radius 
+
+    char **matrix; // matrix
+
+} cluster_t;
+
+typedef struct {
+    int rb; 
+    int k; 
+    int w; 
+} param_t; 
+
+typedef struct {
+    int x;
+    int y; 
+} coord_t; 
+
+cluster_t *init_graph(int rc); 
